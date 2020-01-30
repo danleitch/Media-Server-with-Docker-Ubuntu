d711c5ce237b23024725f86296c5f862dfd5c87b
Daniel <40886941+Thunder-Chief@users.noreply.github.com>
2020-01-30 21:20:48 +0200

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -50,17 +50,17 @@ docker run   --name=sonarr   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London  
 
 echo "INSTALLING RADARR"
 {
-docker run   --name=radarr   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -e UMASK_SET=022    -p 7878:7878   -v ~/Documents/Docker/radarr/data:/config   -v /:/Data   --restart unless-stopped 
+docker run   --name=radarr   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -e UMASK_SET=022    -p 7878:7878   -v ~/Documents/Docker/radarr/data:/config   -v /:/Data   --restart unless-stopped linuxserver/radarr:latest
 }&> /dev/null
 
 echo "INSTALLING qBittorrent"
 {
-docker run   --name=qbittorrent   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -e UMASK_SET=022   -e WEBUI_PORT=8080   -p 6881:6881   -p 6881:6881/udp   -p 8080:8080   -v ~/Documents/Docker/qbittorrent/config:/config   -v /:/Data   --restart unless-stopped   linuxserver/qbittorrent:latest
+docker run   --name=qbittorrent   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -e UMASK_SET=022   -e WEBUI_PORT=8086   -p 6881:6881   -p 6881:6881/udp   -p 8086:8086   -v ~/Documents/Docker/qbittorrent/config:/config   -v /:/Data   --restart unless-stopped   linuxserver/qbittorrent:latest
 }&> /dev/null
 
 echo "INSTALLING SABnzbd"
 {
-docker run   --name=sabnzbd   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -p 8888:8080   -p 9090:9090   -v ~/Documents/Docker/sabnzbd/data:/config   -v /:/Data   --restart unless-stopped   linuxserver/sabnzbd:latest
+docker run   --name=sabnzbd   -e PUID=1000   -e PGID=1000   -e TZ=Europe/London   -p 8080:8080   -p 9090:9090   -v ~/Documents/Docker/sabnzbd/data:/config   -v /:/Data   --restart unless-stopped   linuxserver/sabnzbd:latest
 }&> /dev/null
 
 
