ef9fea30ce5c78a034d5672cbec0bd5e00381400
Daniel <40886941+Thunder-Chief@users.noreply.github.com>
2020-02-03 12:18:42 +0200

    Updating -  Docker Compose to 1.25.4
    https://github.com/docker/compose/releases

--- Bash.sh
+++ Bash.sh
@@ -18,7 +18,7 @@ cd ~ ; pwd
 
 echo "Installing your new awesome Docker Services"
 {
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker ${USER}
 mkdir ~/docker
